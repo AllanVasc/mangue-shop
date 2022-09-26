ce73ed94853d3d57f5c41b29fcfa694e0554431a
Allan Soares Vasconcelos <allsovas@motorola.com>
2022-09-25 22:36:39 -0300

    Created some common classes in styles.css and fixed designs

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent} from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'forgot-password', component: ForgotPasswordComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
