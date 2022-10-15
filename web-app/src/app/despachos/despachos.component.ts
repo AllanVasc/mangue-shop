30a1eb4760d59b010a68b2b7ec355e0cd822dfa8
João Paulo Rocha <jprocha@MacBook-Air-de-JP.local>
2022-10-15 19:57:17 -0300

    verificacao de codigos de despacho

--- web-app/src/app/despachos/despachos.component.ts
+++ web-app/src/app/despachos/despachos.component.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+
+import { 
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators, 
+} from '@angular/forms';
 import { Fornecedor } from '../fornecedor';
 import { FornecedorService } from '../services/fornecedor.service';
+
 
 @Component({
   selector: 'app-despachos',
   templateUrl: './despachos.component.html',
   styleUrls: ['./despachos.component.css']
 })
+
 export class DespachosComponent implements OnInit {
   fornecedor: Fornecedor = new Fornecedor();
+  despachoForm: FormGroup;
+  codinexistente: Boolean = false;
+  despachavel: Boolean = false;
+  pacote = {
+    email: "",
+    codigo: "",
+  };
 
-  constructor(private fornecedorService: FornecedorService) { }
+  constructor(private fornecedorService: FornecedorService) { 
+    this.despachoForm = new FormGroup({
+      codigo: new FormControl(),
+    });
+  }
+
+  despachar(): void {
+    this.pacote.email = this.fornecedor.email;
+    this.pacote.codigo = this.despachoForm.value.codigo;
+
+    this.fornecedorService
+      .despachar(this.pacote)
+      .then((result) => {
+        if (result) {
+          this.despachavel = true;
+        }
+        else {this.codinexistente = true;}
+      })
+      .catch(() => (this.codinexistente = true));
+  }
 
   ngOnInit() {
     this.fornecedorService.getFornecedor()
